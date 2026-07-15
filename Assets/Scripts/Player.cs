787448267fe7553e63530be73a5d11a4b75578ce
GaelGaoro2097 <72483889+GaelGaoro2097@users.noreply.github.com>
2026-07-14 20:33:17 -0600

    Enemigo, Sniper
    Cambio en el Player, en los enemigos y agregamos a un sniper

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,9 +16,11 @@ public class Player : MonoBehaviour
     private Health health;
     private Rigidbody rb;
     public float CurrentHealth => health.CurrentHealth;
+    private FirstPersonMovement firstPersonMovement;
     private Gun currentGun;
     private void Awake()
     {      
+    firstPersonMovement = GetComponent <FirstPersonMovement>();
      rb = GetComponent<Rigidbody>();
      health = GetComponent<Health>();
     }
@@ -59,6 +61,7 @@ public class Player : MonoBehaviour
     {
         Vector3 pushDirection = (transform.position - enemy.position).normalized;
         rb.AddForce(pushDirection * force, ForceMode.Impulse);
+        firstPersonMovement.AddKnockback(pushDirection,force);
     }
     public void Die()
     {
