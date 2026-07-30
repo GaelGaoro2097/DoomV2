8afd7b31f24d44ae339bbbb79c6ca59de7adc892
GarlGaoro2097 <72483889+GaelGaoro2097@users.noreply.github.com>
2026-07-30 14:36:35 -0600

    Cambios de la clase

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEvent OnPKeyPressed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-public bool LeftButtonPressed{ get; private set;}
+    public bool LeftButtonPressed{ get; private set;}
 public bool LeftButtonHeld{ get; private set;}
 public bool RightButtonPressed{ get; private set;}
 
 private void Update()
-    {
+{
         LeftButtonPressed=Input.GetMouseButtonDown(0);
         LeftButtonHeld=Input.GetMouseButton(0);
         RightButtonPressed = Input.GetMouseButtonDown(1);
-    }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnPKeyPressed.Invoke();
+        }
+}
 
 
 }
